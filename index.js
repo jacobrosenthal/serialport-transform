588e8f34fac8289ce4d75d10a6172c688a9b451f
Jacob Rosenthal <jakerosenthal@gmail.com>
2014-12-17 19:12:27 -0800

    wrong package name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var transformTools = require('browserify-transform-tools');
 module.exports = transformTools.makeRequireTransform("requireTransform",
   {evaluateArguments: true},
   function(args, opts, cb) {
-      if (args[0] === "node-serialport") {
+      if (args[0] === "serialport") {
           return cb(null, "require('browser-serialport')");
       } else {
           return cb();
